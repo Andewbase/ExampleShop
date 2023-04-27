4ac9f705b4e2ee70b15221bcd861196b2c97986b
nova_ <nova_09@mail.ru>
2023-04-27 18:25:30 +1000

    In the way of creation

--- app/src/main/java/com/example/exampleshop/app/screens/main/product/ListProductViewModel.kt
+++ app/src/main/java/com/example/exampleshop/app/screens/main/product/ListProductViewModel.kt
@@ -1,14 +1,11 @@
 package com.example.exampleshop.app.screens.main.product
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.exampleshop.app.ConstToken.ADMIN_TOKEN
 import com.example.exampleshop.app.model.Result
 import com.example.exampleshop.app.model.accounts.AccountsRepository
 import com.example.exampleshop.app.model.products.ProductsRepository
 import com.example.exampleshop.app.model.products.entities.Product
-import com.example.exampleshop.app.model.settings.AppSettings
 import com.example.exampleshop.app.screens.base.BaseViewModel
 import com.example.exampleshop.app.utils.logger.Logger
 import com.example.exampleshop.app.utils.share
@@ -19,7 +16,6 @@ import javax.inject.Inject
 @HiltViewModel
 class ListProductViewModel @Inject constructor(
     private val productsRepository: ProductsRepository,
-    private val appSettings: AppSettings,
     accountsRepository: AccountsRepository,
     logger: Logger
 ): BaseViewModel(accountsRepository, logger) {
